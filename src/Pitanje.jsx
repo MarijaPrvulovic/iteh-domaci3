3f02c30e27d9b36eb2fd9ff9c7e5be5576fc7b25
MarijaPrvulovic <marijaprvulovic4@gmail.com>
2023-05-22 23:49:30 +0200

    omoguceno cuvanje rezultata korisnika kroz tabelu

--- src/Pitanje.jsx
+++ src/Pitanje.jsx
@@ -24,7 +24,7 @@ const Pitanje = ({ pitanja,setSkor }) => {
 
     setTimeout(() => {
       if (jePoslednjePitanje) {
-        navigate('/kraj');
+        navigate('/krajb');
       } else {
         const nextQuestionId = Number(id) + 1;
         navigate(`/kviz/${nextQuestionId}`);
--- src/Rezultati.jsx
+++ src/Rezultati.jsx
@@ -1,15 +1,66 @@
-import React, { useState } from 'react';
- 
+import React, { useState, useEffect } from 'react';
 
 function Rezultati() {
+  const [rezultati, setRezultati] = useState([]);
+  const [prikaziPrompt, setPrikaziPrompt] = useState(true);
+  const [promptPrikazan, setPromptPrikazan] = useState(false);
 
-  
+  const handleUnesiNadimak = (score) => {
+    const nadimak = window.sessionStorage.getItem("nadimak")
+    if( !nadimak){
+        const nadimak = window.prompt('Unesite svoj nadimak:');
+        window.sessionStorage.setItem("nadimak",nadimak)
+    }
+   
+    if (nadimak) {
+      const noviRezultat = {
+        id: Date.now(),
+        nadimak,
+        score,
+      };
+      setRezultati((prevRezultati) => [...prevRezultati, noviRezultat]);
+    }
+  };
 
+  useEffect(() => {
+    if (prikaziPrompt && !promptPrikazan) {
+      const score = window.sessionStorage.getItem('score');
+      if (score) {
+        handleUnesiNadimak(score);
+        setPromptPrikazan(true);
+      }
+      setPrikaziPrompt(false);
+    }
+  }, [prikaziPrompt, promptPrikazan]);
+
+  const renderTabelaRezultata = () => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nadimak</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rezultati.map((rezultat) => (
+            <tr key={rezultat.id}>
+              <td>{rezultat.id}</td>
+              <td>{rezultat.nadimak}</td>
+              <td>{rezultat.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
   return (
-     <div>
-        {window.sessionStorage.getItem("score")}
-     </div>
+    <div>
+      <h2>Rezultati</h2>
+      {rezultati.length > 0 && renderTabelaRezultata()}
+    </div>
   );
 }
 
